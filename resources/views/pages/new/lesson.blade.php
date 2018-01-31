9d78d3ba0bf8eae869ae54fb0f4e1a95e290caed
Cali Castle <cali@calicastle.com>
2018-01-31 15:58:18 -0800

    Add pages

---
+++ resources/views/pages/new/lesson.blade.php
@@ -0,0 +1,11 @@
+@extends('partials.base')
+
+@section('title', 'Lesson Changes')
+
+@section('content')
+<div class="container-large">
+	<h2 class="margin-v section-heading">
+		New lesson components: What’s different across the lessons?
+	</h2>
+</div>
+@endsection
---
+++ resources/views/pages/new/program.blade.php
@@ -0,0 +1,56 @@
+@extends('partials.base')
+
+@section('title', 'Program Changes')
+
+@section('content')
+	<h2 class="heading-2 margin-v">
+		What’s new in Strong Kids and Strong Teens?
+	</h2>
+	<div class="container">
+		<h2 class="margin-v section-heading">
+			Programmatic-level changes: How do the programs differ overall?
+		</h2>
+		<p class="margin-v">
+			<b>We better address issues that students increasingly experience in our global society.</b> We integrate
+			sociocultural issues in discussions of emotional awareness, expression, and empathy. Because with whom and
+			where we express our emotions can be just as important as how we express our emotions, we discuss emotional
+			expression in terms of not just our actions but also the context or setting. Throughout the programs, we use
+			more current and authentic examples and reference problems that students today are likely to experience,
+			such as social networking dilemmas, stereotypes, and other complex social situations. We use "twists" in
+			examples, which change a small aspect of the example, thereby altering the way students perceive the
+			situation and the outcome. We also include more modern graphics to pique the interest of students and help
+			ground the more abstract ideas.
+		</p>
+		<p class="margin-v">
+			<b>Build a tolerance for discomfort and reduce reactivity.</b> Distressing emotions such as anger or sadness
+			are discussed in this edition using a slightly different orientation. Rather than conveying only the message
+			that uncomfortable feelings are something to change or fix, we emphasize the value of simply listening to
+			what our bodies are communicating through these emotions. Also, to better address SEL issues associated with
+			students who internalize their emotions, we discuss both the potential negative consequences of acting out
+			and withdrawing, hiding, and repressing our emotions.
+		</p>
+		<p class="margin-v">
+			<b>We added a new lesson, Positive Living!</b> This new lesson promotes a sense of personal responsibility
+			over daily behaviors and actions and promotes the development of healthy habits across school, home, and
+			community settings. To build on these concepts, the next lesson is centered on creating strong, SMART
+			(specific, measurable, attainable, relevant, and timely) goals.
+		</p>
+	</div>
+	<div class="container">
+		<h2 class="margin-v section-heading">
+			Changes across the levels: How does Strong Kids—Grades 3–5 differ from Strong Kids—Grades 6–8, and how does
+			Strong Kids—Grades 6–8 differ from Strong Teens?
+		</h2>
+		<p class="margin-v">
+			The new programs have been revised to better target, differentiate, and address developmental considerations
+			of SEL. The SEL topics bring different contexts and different meanings for students as they age. Making a
+			responsible decision can present more complex considerations for an adolescent than for a student in third
+			grade. Thus, although the major objectives remain unchanged across the levels, the ways in which the lessons
+			approach the objectives are adjusted to fit the development of the student. As the levels advance, the
+			instruction and examples become increasingly sophisticated and abstract. Whereas Strong Kids focuses on more
+			concrete concepts and issues relevant to elementary and middle school, Strong Teens addresses more abstract
+			concepts and uses examples with more complex social situations and issues that are more relevant to teens,
+			such as postsecondary planning.
+		</p>
+	</div>
+@endsection
