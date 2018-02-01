64b137abbef56e2d96ba86cb89d4f6ee500949e7
Cali Castle <cali@calicastle.com>
2018-01-31 23:29:14 -0800

    Update SEO keywords

--- resources/views/partials/base.blade.php
+++ resources/views/partials/base.blade.php
@@ -6,9 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<meta name="author" content="">
-	<meta name="description" content="">
-	<meta name="keywords" content="">
+	<!-- <meta name="description" content=""> -->
+	<meta name="keywords" content="SEL, social and emotional learning, SEL for student, Strong Kids, Strong Teens, Strong Start, resiliency">
 
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
